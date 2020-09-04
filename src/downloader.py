9a321e3aff35aef748e099c68112f609ce4e30dd
ImportTaste <53661808+ImportTaste@users.noreply.github.com>
2020-09-03 19:11:48 -0500

    Override urllib3.HTTPResponse._update_chunk_length

--- src/downloader.py
+++ src/downloader.py
@@ -5,6 +5,9 @@ import os
 import requests
 from tqdm import tqdm
 
+# Import for fixing _update_chunk_length method
+import urllib3
+
 
 def download(data, output_dir, stream, show_already_exists):
     """Download all files referenced in data"""
@@ -57,3 +60,23 @@ def download(data, output_dir, stream, show_already_exists):
         except ImportError:
             print('You need the `{0}` module to download Periscope videos'.format('pyriscope'))
         pass
+
+
+# Fixes one of the urllib3.HTTPResponse private methods
+# https://github.com/psf/requests/issues/4248
+def _update_chunk_length(self):
+    # First, we'll figure out length of a chunk and then
+    # we'll try to read it from socket.
+    if self.chunk_left is not None:
+        return
+    line = self._fp.fp.readline()
+    line = line.split(b';', 1)[0]
+    line = (len(line)>0 and line or "0")    # THIS IS THE FIX
+    try:
+        self.chunk_left = int(line, 16)
+    except ValueError:
+        # Invalid chunked protocol response, abort.
+        self.close()
+        raise httplib.IncompleteRead(line)
+
+urllib3.HTTPResponse._update_chunk_length = _update_chunk_length
