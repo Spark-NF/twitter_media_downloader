4f267b061c975daa64d9331cc73524978846b038
Sam Pham <sam.h.pham@gmail.com>
2021-02-06 01:19:41 +0000

    Can get userIds from a CSV

--- src/args.py
+++ src/args.py
@@ -19,7 +19,7 @@ def parse_date(date):
 def parse_args(args):
     """Parse the arguments passed to twitter_media_downloader using argparse."""
     parser = argparse.ArgumentParser(description='Twitter media downloader.')
-    parser.add_argument('userid', type=str, nargs='+', help='the account name or ID')
+    parser.add_argument('userid', type=str, nargs='*', help='the account name or ID')
     parser.add_argument('-o', '--output', type=str, metavar='DIR', help='the output directory', default='out')
     parser.add_argument('-f', '--format', type=str, metavar='FORMAT', help='the filename format', default='[%date%] %filename%.%ext%')
     parser.add_argument('-s', '--image-size', type=str, metavar='IMAGE_SIZE', help='the preferred image size to download', default='medium', choices=['thumb', 'small', 'medium', 'large', 'orig'])
@@ -31,6 +31,7 @@ def parse_args(args):
     parser.add_argument('-l', '--likes', help='download user liked medias', action='store_true')
     parser.add_argument('-u', '--userid', help='append userid to output directory', action='store_true', dest='o_userid')
     parser.add_argument('-q', '--quiet', help='disable output', action='store_true')
+    parser.add_argument('-c', '--csv', help='load users from csv')
     return parser.parse_args(args)
 
 
--- twitter_media_downloader.py
+++ twitter_media_downloader.py
@@ -3,6 +3,7 @@
 
 import os.path
 import sys
+import csv
 
 # Import local functions
 from src.args import parse_args, parse_file_arg
@@ -15,7 +16,22 @@ from src.downloader import download
 if __name__ == '__main__':
     # Parse program arguments
     args = parse_args(sys.argv[1:])
-    user_ids = parse_file_arg(args.userid)
+
+    user_ids = []
+
+    if (len(args.userid) == 0 and args.csv is None) or (len(args.userid) > 0 and args.csv is not None):
+        raise Exception('You need to either provider a csv or list of userIds')
+    elif len(args.userid) > 0:
+        user_ids = parse_file_arg(args.userid)
+    elif args.csv is not None:
+        if not os.access(args.csv, os.R_OK):
+            raise Exception('Could not read file ' + args.csv)
+
+        with open(args.csv) as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+
+            for row in csv_reader:
+                user_ids.append(row['userId'])
 
     # Twitter OAuth
     auth = get_oauth('.oauth.json')
